7be98bb8c22fd9fe272a48b10e91b035ec6c4f41
Adam Stambler <asher@asher-mbp.(none)>
2009-12-12 21:32:08 -0500

    I dont really know why this works as apposed to before, but it does...

--- main.cpp
+++ main.cpp
@@ -34,12 +34,12 @@ int main(void)
 	scanner.setMaxScanAngle(120);
 
 	while(1==1){
-	//	if (Serial.available()){ // get new byte, parse if neccesary
+		if (Serial.available()){ // get new byte, parse if neccesary
 
-		//}
-		while(1==1)		{
-			scanner.scan2();
 		}
+			scanner.scan2();
+			_delay_ms(1);
+
 
 	}
 
